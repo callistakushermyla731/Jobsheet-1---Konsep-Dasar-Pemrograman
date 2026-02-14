03b2473b80dab166140518f070d99be5fe1441e6
Callista Dinar Kushermyla <callistakushermyla731@gmail.com>
2026-02-14 18:08:36 +0700

    Perulangan

---
+++ perulangan.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class perulangan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan NIM : ");
+        long nim = sc.nextLong();
+
+        int n = (int)(nim % 100);
+
+        if (n < 10) {
+            n += 10;
+        }
+
+        System.out.println("Nilai n : " + n);
+        System.out.print("Output : ");
+
+        for (int i = 1; i <= n; i++){
+            if (i == 10 || i == 15) {
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                System.out.print("# ");
+            } else if (i % 2 == 1) {
+                System.out.print("* ");
+            } else {
+                System.out.print(i + " ");
+            }
+                
+        }
+                
+    }
+}
+
+
